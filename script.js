db1cd865eeb97a3ea00692f4a21945ad298aa356
Abu Zaid <zaid70979@gmail.com>
2023-12-16 22:12:56 +0530

    fix javaScript

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ button.addEventListener('click',()=>{
     }
     h2.innerHTML = `Your current balance:- ${UserDetails[name.value].balance}`
 })
+// let UserDetails={
+//     alakhSir:{
+//         balance:2510000
+//     },
+//     Zaid:{
+//         balance:7510
+//     },
+//     Rahul:{
+//         balance:3500
+//     }
+// } 
+
+// let name=document.getElementById('name')
+// let action = document.getElementById('action')
+// let button=document.querySelector('button')
+// let amount=document.getElementById('amount')
+// let select=document.querySelector('#action')
+// let h2=document.querySelector('h2')
+// button.addEventListener('click',()=>{
+
+//     if(select.value=='deposit'){
+//         UserDetails[name.value].balance += Number(amount.value)
+//     }
+//     if(select.value=='withdrawal'){
+//         UserDetails[name.value].balance -= Number(amount.value)
+//     }
+//     h2.innerHTML = `Your current balance:- ${UserDetails[name.value].balance}`
+// })
